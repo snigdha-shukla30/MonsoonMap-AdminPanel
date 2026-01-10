dc677572fc58c74354149665df58a0fbef1b098c
Snigdha Shukla <snigdhashukla0@gmail.com>
2026-01-10 20:20:08 +0530

    initial commit

---
+++ src/app/report/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Sidebar from "../../Components/ui/Sidebar";
+import Reports from "../../Components/report/report";
+
+export default function ReportsPage() {
+  return (
+    <div className="flex h-screen bg-[linear-gradient(180deg,rgb(174,209,255)_0%,rgb(126,145,169)_100%)]">
+      <Sidebar />
+      <Reports />
+    </div>
+  );
+}
